42b1e2315acf58a8f83fc529ebd8bf3540e8de47
a.pe <a.pe@reply.it>
2022-11-04 10:44:10 +0100

    add md bootsrap and navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  menuExpanded = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleMenu() {
+    setTimeout(() => {
+      this.menuExpanded = !this.menuExpanded;
+    }, 0);
   }
+
+  @HostListener('window:click', ['$event'])
+  listenToOutsideClick(event: PointerEvent) {
+    const target = event.target as HTMLElement;
+    const isToggler = target.getAttribute('id') === 'navbarDropdown'
+    if (!this.menuExpanded || isToggler) {
+      return;
+    }
+
+    this.menuExpanded = false;
+  };
 
 }
